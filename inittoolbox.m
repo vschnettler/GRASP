ca52c95a2147eee27249e0c9ee99e184060e9500
vschnettler <vale.schnettler@gmail.com>
2022-09-21 15:56:04 -0300

    add function

---
+++ inittoolbox.m
@@ -0,0 +1,39 @@
+function inittoolbox 
+
+base=pwd;
+
+addpath(base);
+addpath([base filesep 'visualization']);
+addpath([base filesep 'visualization' filesep 'simulation_viz']);
+addpath([base filesep 'visualization' filesep 'simulation_viz' filesep 'tests']);
+addpath([base filesep 'visualization' filesep 'simulation_viz' filesep 'simulation_viz']);
+addpath([base filesep 'visualization' filesep 'simulation_viz' filesep 'tests' filesep 'test_files']);
+addpath([base filesep 'tutorials']);
+addpath([base filesep 'temp']);
+addpath([base filesep 'patterns']);
+addpath([base filesep 'matlab_code']);
+addpath([base filesep 'matlab_code' filesep 'tests']);
+addpath([base filesep 'matlab_code' filesep 'tests' filesep 'patternFxns']);
+addpath([base filesep 'matlab_code' filesep 'tests' filesep 'patternFxns' filesep 'testFiles']);
+addpath([base filesep 'matlab_code' filesep 'tests' filesep 'ensembleFxns']);
+addpath([base filesep 'matlab_code' filesep 'tests' filesep 'ensembleFxns' filesep 'testFiles']);
+addpath([base filesep 'matlab_code' filesep 'tests' filesep 'analysisFxns']);
+addpath([base filesep 'matlab_code' filesep 'tests' filesep 'analysisFxns' filesep 'testFiles']);
+addpath([base filesep 'matlab_code' filesep 'tests' filesep 'analysisFxns' filesep 'testFiles' filesep 'reactions']);
+addpath([base filesep 'matlab_code' filesep 'patternFxns']);
+addpath([base filesep 'matlab_code' filesep 'ensembleFxns']);
+addpath([base filesep 'matlab_code' filesep 'analysisFxns']);
+addpath([base filesep 'io']);
+addpath([base filesep 'io' filesep 'output']);
+addpath([base filesep 'io' filesep 'input']);
+addpath([base filesep 'examples']);
+addpath([base filesep 'docs']);
+addpath([base filesep 'docs' filesep 'images']);
+addpath([base filesep 'docs' filesep 'api_doc']);
+addpath([base filesep 'docs' filesep 'api_doc' filesep 'simulation_viz']);
+addpath([base filesep 'docs' filesep 'api_doc' filesep 'patternFxns']);
+addpath([base filesep 'docs' filesep 'api_doc' filesep 'ensembleFxns']);
+addpath([base filesep 'docs' filesep 'api_doc' filesep 'analysisFxns']);
+addpath([base filesep 'docs' filesep '.vscode']);
+
+end
